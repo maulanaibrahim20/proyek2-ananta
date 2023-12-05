d07f92caf2db7acae318fda52a1261ec426517b0
maulibrahim20 <maulibrahim20@student.polindra.ac.id>
2023-12-05 22:05:25 +0700

    first commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class RegisterController extends Controller
 {
@@ -19,14 +20,16 @@ class RegisterController extends Controller
                 'name' => $request->name,
                 'username' => str::slug($request->name),
                 'email' => $request->email,
-                'password' => bcrypt('password'),
+                'password' => $request->password,
                 'no_tlp' => $request->no_tlp,
                 'alamat' => $request->alamat,
                 'role_id' => '3',
 
             ]);
+            Alert::success('Success', 'Data Berhasil Di Tambahkan');
             return redirect('/login')->with('success');
         } catch (\Exception $e) {
+            Alert::error('Gagal', 'Data Gagal Di Tambahkan');
             return back()->with('error' . $e->getMessage());
         }
     }
